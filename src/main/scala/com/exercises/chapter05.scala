1bd1053bac6762fc35914799e84240acdd92d246
Ben Cavins <cavinsb@colorado.edu>
2016-05-28 18:12:13 -0600

    Add ch05 template

---
+++ src/main/scala/com/exercises/chapter05.scala
@@ -0,0 +1,37 @@
+
+object chapter05 {
+
+    def exercise01 {}
+
+    def exercise02 {}
+
+    def exercise03 {}
+
+    def exercise04 {}
+
+    def exercise05 {}
+
+    def exercise06 {}
+
+    def exercise07 {}
+
+    def exercise08 {}
+
+    def exercise09 {}
+
+    def exercise10 {}
+
+    def main(args: Array[String]) {
+        exercise01
+        exercise02
+        exercise03
+        exercise04
+        exercise05
+        exercise06
+        exercise07
+        exercise08
+        exercise09
+        exercise10
+    }
+
+}
